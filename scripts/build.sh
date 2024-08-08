c0d7eafed2840e26ec04299916350e90a2ddbc3c
j-aub <j-aub@users.noreply.github.com>
2024-08-08 08:48:19 -0400

    init

---
+++ scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -eu
+
+# name of modpack to use in filenames
+NAME="modpack-test"
+# $1 is the version/commit
+VERSION="$1"
+
+# rename the packwiz binary to something sane
+mv 'Linux 64-bit x86' packwiz
+# ensure the packwiz binary is executable
+chmod +x packwiz
+# ensure output directory exists
+mkdir dist
+
+# create the modpacks
+# client curseforge
+packwiz curseforge export -o "dist/${NAME}-curseforge-${VERSION}.zip"
+# server curseforge
+packwiz curseforge export -o "dist/${NAME}-curseforge-server-${VERSION}.zip" -s server
+# modrinth
+packwiz curseforge export -o "dist/${NAME}-modrinth-${VERSION}.mrpack"
+# we now create a prismlauncher dummy modpack
